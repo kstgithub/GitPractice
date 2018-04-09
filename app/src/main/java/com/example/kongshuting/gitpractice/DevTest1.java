47416f186c23d6e98440a0d8188747f6fa2cc0ce
kongshuting <kongshuting@jd.com>
2018-04-09 11:29:24 +0800

    添加两个测试类。
    (cherry picked from commit e7a9fd4)

---
+++ app/src/main/java/com/example/kongshuting/gitpractice/DevTest1.java
@@ -0,0 +1,9 @@
+package com.example.kongshuting.gitpractice;
+
+/**
+ * DevTest1
+ * Created by kongshuting on 2018/4/9.
+ */
+
+public class DevTest1 {
+}
---
+++ app/src/main/java/com/example/kongshuting/gitpractice/DevTest2.java
@@ -0,0 +1,9 @@
+package com.example.kongshuting.gitpractice;
+
+/**
+ * DevTest2
+ * Created by kongshuting on 2018/4/9.
+ */
+
+public class DevTest2 {
+}
